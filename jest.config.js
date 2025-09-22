8e85bf4d856ae1dea975223ca9a057e7bb38bdb0
Hardik Shrestha <hardik.ho@gmail.com>
2025-09-22 03:33:45 -0400

    Move cache lab workspace under src apps

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
   roots: ['<rootDir>/src'],
+  testPathIgnorePatterns: ['<rootDir>/src/apps/cache-lab/tests/'],
   moduleNameMapper: {
     '\\.(css|less|scss)$': '<rootDir>/src/testUtils/styleMock.js',
   },
